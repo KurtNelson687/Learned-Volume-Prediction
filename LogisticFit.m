34793228b256572be3640c9e238f7e1dafc12e14
kurt nelson <kurtnelson@kurts-mbp.gateway.pace.com>
2017-11-21 01:30:59 -0800

    added classification predictions - these are made in an mfile called LogisticRegression.m

---
+++ LogisticFit.m
@@ -0,0 +1,10 @@
+function criterion = LogisticFit(X_train,y_train,X_test,y_test)
+ ypred = classify(X_test,X_train,y_train,'linear'); %this can be used for
+% what I believe is softmax
+
+%lda = fitcdiscr(X_train,y_train,'DiscrimType','linear'); %Use this for different discriminant models
+%[ypred,~,~] = predict(lda,X_test);
+
+criterion = sum(~(y_test==ypred));
+end
+
