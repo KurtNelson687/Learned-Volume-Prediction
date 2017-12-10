2992a75e83d7cc544fe697e473b6b650e75e154d
kurt nelson <kurtnelson@kurts-mbp.gateway.pace.com>
2017-12-09 23:01:52 -0800

    tree update

--- Regression/GeneralRegressionFits.m
+++ Regression/GeneralRegressionFits.m
@@ -4,7 +4,7 @@ addpath('./functions');
 
 %This is the tpye of model to test - currently available options: 'OLS',
 %'Lasso', 'wt_percentDiff', and 'wt_local'
-modelType = 'Ridge';
+modelType = 'wt_local';
 RoundPrediction = 0; % choose to round the output of linear regression
 
 %% create possible feature sets
